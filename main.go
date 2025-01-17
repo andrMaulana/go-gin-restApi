033df6fe02e0140ff248c3809907a58d94f1e012
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-17 23:55:07 +0700

    feat: ✨ register route find posts

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/andrMaulana/go-gin-restApi/controllers"
+	"github.com/andrMaulana/go-gin-restApi/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// inisialisasi gin
 	router := gin.Default()
 
+	// panggil koneksi database
+	models.ConnectDatabase()
 	// membuat route dengan method get
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
 		})
 	})
+
+	router.GET("/api/posts", controllers.FindPosts)
 
 	router.Run(":3000")
 }
